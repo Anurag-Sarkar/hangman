68494491f86b44f4ec9e4ed8bef87c0026407ee1
Anurag <sarkaranurag104@gmail.com>
2022-05-01 19:38:09 +0530

    lol

--- hangman.py
+++ hangman.py
@@ -24,28 +24,32 @@ def printer(l):
 
 word = wordpicker()
 tries = 0
-chance = 7
+chance = 8
+g = []
 l = ["_" for i in range(len(word))]
 printer(l)
-
-while True:
+print(word)
+while tries != chance:
 
     if word.upper() == "".join(l):
         print("Yaaa! you saved me!")
         break
 
-    guess = input("\nEnter Guess: ")
+    guess = input("\nEnter Guess: ",)
+    g.append(guess)
+    if len(g) > 0:
+        print("(",end="")
+        for i in g:
+            print(i,end=" ") 
+        print(")",end="")
+    print()
 
     if guess in word:    
         index = [i for i, l in enumerate(word) if l == guess]
         for i in index:
             l[i] = guess.upper()
         printer(l)
-        
-    if (tries == chance):
-        print("I trusted you...")
-        print("the word was,",word)
-        break
+        print(tries)
 
     if guess not in word:
         tries+=1
@@ -53,3 +57,7 @@ while True:
         print()
         print()
         printer(l)
+        print(tries)
+else:
+    print("/ni trusted you...")
+    print("The word was,",word)
